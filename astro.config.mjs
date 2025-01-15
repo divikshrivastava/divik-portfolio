60623772b5949a602fee9cb267b10e9249faa8e1
divikshrivastava <divikshrivastava@gmail.com>
2025-01-15 21:22:49 +0530

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,14 @@ import netlify from "@astrojs/netlify";
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
+import { EventEmitter } from 'events';
 
 import solidJs from "@astrojs/solid-js";
 import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
 
 import svelte from "@astrojs/svelte";
 
+EventEmitter.defaultMaxListeners = 11;
 // https://astro.build/config
 export default defineConfig({
   site: "https://gianmarco.xyz/",
